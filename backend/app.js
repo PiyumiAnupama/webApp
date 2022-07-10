1cac4d6c5a89a682f155a36b123348b0bad52e54
PiyumiAnupama <96447@fhss.sjp.ac.lk>
2022-07-11 01:22:29 +0530

    connect api

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/user-routes";
 
 const app = express();
+app.use(express.json());
+app.use("/api/user", router);
 
-app.use("/api", (req,res,next)=>{
-    res.send("hello world")
-})
+
 mongoose.connect("mongodb+srv://Anu:Anu90@cluster0.h0hga.mongodb.net/WebApplication?retryWrites=true&w=majority")
 .then(() => app.listen(5000))
 .then(() =>
