a12e7a67c91d398c488a3d3e9abf6f62b9e98f88
PiyumiAnupama <96447@fhss.sjp.ac.lk>
2022-07-11 15:24:33 +0530

    create blog api

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 
 const app = express();
 app.use(express.json());
 app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
 
 
 mongoose.connect("mongodb+srv://Anu:Anu90@cluster0.h0hga.mongodb.net/WebApplication?retryWrites=true&w=majority")
--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import User from "../model/User";
+import bcrypt from "bcryptjs";
 
 export const getAllUser = async (req, res, next) => {
     let users;
@@ -27,12 +28,14 @@ export const signup = async (req, res, next) => {
           .status(400)
           .json({ message: "User Already Exists! Login Instead" });
       }
-
+      const hashedPassword = bcrypt.hashSync(password);
       const user = new User({
         name,
         email,
-        password,
+        password: hashedPassword,
+        blogs:[],
       });
+     
 
       try {
         await user.save();
@@ -40,3 +43,22 @@ export const signup = async (req, res, next) => {
         return console.log(err);
       } return res.status(201).json({ user });
     }
+    export const login = async (req, res, next) => {
+        const { email, password } = req.body;
+        let existingUser;
+        try {
+          existingUser = await User.findOne({ email });
+        } catch (err) {
+          return console.log(err);
+        }
+        if (!existingUser) {
+          return res.status(404).json({ message: "Couldnt Find User By This Email" });
+        }
+        const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect Password" });
+  }
+  return res
+    .status(200)
+    .json({ message: "Login Successfull", });
+};
