0b7afc641494131664c830ceb1d53e3574ecd79f
PiyumiAnupama <96447@fhss.sjp.ac.lk>
2022-07-12 15:26:43 +0530

    connected backend

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,54 +1,59 @@
-import { Button, TextField, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store";
+import { useNavigate } from "react-router-dom";
 
-const Auth =()=>{
-    const [inputs, setInputs] = useState({
-        name: "",
-        email: "",
-        password: "",
-      });
-    const [isSignup, setIsSignup] = useState(false);
-    const handleChange = (e) => {
-        setInputs((prevState) => ({
-          ...prevState,
-          [e.target.name]: e.target.value,
-        }));
-      };
+const Auth = () => {
+  const naviagte = useNavigate();
+  const dispath = useDispatch();
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [isSignup, setIsSignup] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const sendRequest = async (type = "login") => {
+    const res = await axios
+    .post(`http://localhost:5000/api/user/${type}`, {
+      name: inputs.name,
+      email: inputs.email,
+      password: inputs.password,
+      })
+      .catch((err) => console.log(err));
+      const data = await res.data;
+      console.log(data);
+      return data;
+  };
 
-      const sendRequest = async (type = "login") => {
-        const res = await axios
-          .post(`http://localhost:5000/api/user/${type}`, {
-            name: inputs.name,
-            email: inputs.email,
-            password: inputs.password,
-          })
-          .catch((err) => console.log(err));
-    
-        const data = await res.data;
-        console.log(data);
-        return data;
-      };
-    
-
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(inputs);
-        if (isSignup) {
-            sendRequest("signup").then(console.log(data)) 
-        }
-        else{
-            sendRequest()
-        .then((data) => localStorage.setItem("userId", data.user._id))
-        }
-      }
-
-    return(
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);  
+    if (isSignup) {
+      sendRequest("signup")
+      .then(() => dispath(authActions.login())).then(() => naviagte("/blogs"))
+        .then(data => console.log(data));
         
-        <div>
-            <form onSubmit={handleSubmit}>
-                <Box maxWidth={400}
+        
+    } else {
+      sendRequest()
+      .then(() => dispath(authActions.login())).then(() => naviagte("/blogs"))
+      .then(data => console.log(data));
+    } 
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          maxWidth={400}
           display="flex"
           flexDirection={"column"}
           alignItems="center"
@@ -57,41 +62,54 @@ const Auth =()=>{
           padding={3}
           margin="auto"
           marginTop={5}
-          borderRadius={5}>
-
-                    <Typography variant="h2" padding={3} textAlign="center">
-                    {isSignup ? "Signup" : "Login"}
-                    </Typography>
-
-                    {isSignup && (<TextField  name="name"
-                     onChange={handleChange}
-                    value={inputs.name}
-                    placeholder="Name"
-                    margin="normal"/>
-
-)}{" "} <TextField name="email"
-                    onChange={handleChange}
-                    value={inputs.email}
-                    placeholder="Email"
-                    margin="normal"/>
-
-                    <TextField name="password"
-                    onChange={handleChange}
-                    value={inputs.password}
-                    placeholder="Password"
-                    margin="normal"/>
-
-                    <Button  type="submit"
+          borderRadius={5}
+        >
+          <Typography variant="h2" padding={3} textAlign="center">
+            {isSignup ? "Signup" : "Login"}
+          </Typography>
+          {isSignup && (
+            <TextField
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+              placeholder="Name"
+              margin="normal"
+            />
+          )}{" "}
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            type={"password"}
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button 
+            type="submit"
             variant="contained"
-            sx={{ borderRadius: 3, marginTop: 3, color:'white' }}
-            >Submit</Button>
+            sx={{ borderRadius: 3, marginTop: 3 , color:"white"}}
+           
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignup(!isSignup)}
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            Change To {isSignup ? "Login" : "Signup"}
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
 
-                    <Button  onClick={() => setIsSignup(!isSignup)} sx={{ borderRadius: 3, marginTop: 3 }}>
-                    Change To {isSignup ? "Login" : "Signup"}
-                    </Button>
-                </Box>
-            </form>
-        </div>
-    )
-}
-export default Auth
+export default Auth;
